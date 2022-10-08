b8ba8dc0b3189bd0ea9b055b77a666fb5f7f40f0
mk-os <72484457+mk-os@users.noreply.github.com>
2022-10-08 15:55:31 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import random
 import discord
 
 token = "MTAyODQzNTczOTk5OTk5Nzk2Mw.G546px.JdXaRLB_5OD9ynk0FeD34dTPMspsj6WQGFZmJU"
+
+def mocked(text):
+    c = []
+    for t in text:
+        r = random.randint(0, 1)
+        if r == 0:
+            c.append(t.lower())
+        else:
+            c.append(t.upper())
+    return ''.join(c)

4bdb34af03e1aff0e377dcb7d6c1971bfac92e56
mk-os <72484457+mk-os@users.noreply.github.com>
2023-03-25 12:28:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import random
 import discord
 
 bot = discord.Client()
-token = "MTAyODQzNTczOTk5OTk5Nzk2Mw.G546px.JdXaRLB_5OD9ynk0FeD34dTPMspsj6WQGFZmJU"
+token = "" # Put your token here 
 
 def mocked(text):
     c = []
